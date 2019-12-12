0f944a34e40644ad15a376206df6a7d0ae461ea5
Sebastian <sebastianariel.higa@globant.com>
2019-12-12 10:17:52 -0300

    First implementation of tds

---
+++ project/src/lib/c-tds/system_lpc1769.c
@@ -0,0 +1,180 @@
+/*--------------------------------------------------------------------*-
+
+    system_lpc1769.c (Released 2019-04)
+
+    Controls system configuration after processor reset.
+    [Two modes supported - "Normal" and "Fail Silent".]
+
+-*--------------------------------------------------------------------*/
+
+
+// Project header
+#include "main.h"
+#include "system_lpc1769.h"
+
+
+// Task headers
+#include "tasks.h"
+
+// ------ Public variable ------------------------------------------
+// In many designs, System_mode_G will be used in other modules.
+// - we therefore make this variable public.
+eSystem_mode System_mode_G;
+
+
+// ------ Private function -----------------------------------------
+void SYSTEM_Identify_Required_Mode(void);
+void SYSTEM_Configure_Required_Mode(void);
+
+
+/*------------------------------------------------------------------*-
+
+    SYSTEM_Init()
+
+    Wrapper for system startup functions.
+
+-*------------------------------------------------------------------*/
+void SYSTEM_Init(void)
+{
+    SYSTEM_Identify_Required_Mode();
+    SYSTEM_Configure_Required_Mode();
+}
+
+
+/*------------------------------------------------------------------*-
+
+    SYSTEM_Identify_Required_Mode()
+
+    Try to work out the cause of the system reset.
+    Set the system mode accordingly.
+
+-*------------------------------------------------------------------*/
+void SYSTEM_Identify_Required_Mode(void)
+{
+	// Set system mode (Normal)
+	System_mode_G = NORMAL;
+}
+
+
+/*------------------------------------------------------------------*-
+
+    SYSTEM_Configure_Required_Mode()
+
+    Configure the system in the required mode.
+
+-*------------------------------------------------------------------*/
+void SYSTEM_Configure_Required_Mode(void)
+{
+	Chip_SetupXtalClocking();
+
+	/* Setup FLASH access to 4 clocks (100MHz clock) */
+	Chip_SYSCTL_SetFLASHAccess(FLASHTIM_100MHZ_CPU);
+
+	SystemCoreClockUpdate();
+
+	/* Initialize GPIO */
+	Chip_GPIO_Init(LPC_GPIO);
+	Chip_IOCON_Init(LPC_IOCON);
+
+	switch (System_mode_G)
+	{
+        default: // Default to "FAIL_SILENT"
+        case FAIL_SILENT:
+        {
+            // Reset caused by WDT
+            // Trigger "fail silent" behavior
+            SYSTEM_Perform_Safe_Shutdown();
+
+            break;
+        }
+
+        case NORMAL:
+        {
+        	// Set up scheduler for 1 ms ticks (tick interval in *ms*)
+            SCH_Init(1);
+
+            /* Initialize WWDT and event router */
+        	Chip_WWDT_Init(LPC_WWDT);
+
+        	//Prepare tasks
+            InitHardware();
+
+            SCH_Add_Task(TareaLeeSerie, 1, 10, 20, 0);
+            SCH_Add_Task(Task_ADC, 1, 10, 20, 0);
+            SCH_Add_Task(Task_SignalProcess, 1, 10, 20, 0);
+            SCH_Add_Task(Task_display, 1, 10, 20, 0);
+
+
+        	// Add tasks to schedule.
+            // Parameters are:
+            // 1. Task name
+            // 2. Initial delay / offset (in ticks)
+            // 3. Task period (in ticks): Must be > 0
+            // 4. Task WCET (in microseconds)
+            // 5. Task BCET (in microseconds)
+
+            // Add GPIO_SWITCH task
+            // SCH_Add_Task(GPIO_SWITCH_Update, 1, 10, 20, 0);
+
+            // Add GPIO_LED task
+            // SCH_Add_Task(GPIO_LED_Update, 1, 10, 20, 0);
+
+            // Add Heartbeat task
+            // SCH_Add_Task(HEARTBEAT_Update, 0, 1000, 20, 0);
+
+            break;
+        }
+	}
+}
+
+
+/*------------------------------------------------------------------*-
+
+    SYSTEM_Perform_Safe_Shutdown()
+
+    Attempt to place the system into a safe state.
+
+    Note: Does not return and may (if watchdog is operational) result
+    in a processor reset, after which the function may be called again.
+    [The rationale for this behavior is that - after the reset -
+    the system MAY be in a better position to enter a safe state.
+    To avoid the possible reset, adapt the code and feed the WDT
+    in the loop.]
+
+-*------------------------------------------------------------------*/
+void SYSTEM_Perform_Safe_Shutdown(void)
+{
+    // Used for simple fault reporting
+    uint32_t Delay, j;
+
+    // Here we simply "fail silent" with rudimentary fault reporting
+    // OTHER BEHAVIOUR IS LIKELY TO BE REQUIRED IN YOUR DESIGN
+    // *************************************
+    // NOTE: This function should NOT return
+    // *************************************
+    // HEARTBEAT_Init();
+
+    while(1)
+	{
+        // Flicker Heartbeat LED to indicate fault
+        for (Delay = 0; Delay < 200000; Delay++) j *= 3;
+        // HEARTBEAT_Update();
+	}
+}
+
+
+void SYSTEM_Change_Mode_Fault(eSystem_mode mode)
+{
+	System_mode_G = mode;
+}
+
+
+eSystem_mode SYSTEM_Get_Mode(void)
+{
+	return System_mode_G;
+}
+
+
+/*------------------------------------------------------------------*-
+  ---- END OF FILE -------------------------------------------------
+-*------------------------------------------------------------------*/
